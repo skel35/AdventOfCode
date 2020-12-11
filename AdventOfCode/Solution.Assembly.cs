164db026d5678e92c456e272e95b082cad436248
Taras Petruk <taras.petruk@gmail.com>
2020-12-11 06:57:06 +0200

    assembly-related helper methods

---
+++ AdventOfCode/Solution.Assembly.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace AdventOfCode
+{
+    partial class Solution
+    {
+        protected (string Op, int Arg)[] ReadProgram()
+        {
+            return ReadLines()
+                .Select(line => line.Split())
+                .Select(a => (Op: a[0], Arg: a[1].ToInt()))
+                .ToArray();
+        }
+
+        protected (int Acc, int Ind) ProcessLine((string Op, int Arg) p, (int Acc, int Ind) c)
+        {
+            var (acc, ind) = c;
+            return p switch
+            {
+                ("nop", _) => (acc, ind + 1),
+                ("acc", { } n) => (acc + n, ind + 1),
+                ("jmp", { } n) => (acc, ind + n),
+                _ => (acc, ind)
+            };
+        }
+    }
+}
