e03008302c9153f06398f7095ad343040c80129a
Taras Petruk <taras.petruk@gmail.com>
2020-12-13 12:20:35 +0200

    day 13 2020 original solution

--- AdventOfCode/2020/Day13.cs
+++ AdventOfCode/2020/Day13.cs
@@ -10,7 +10,72 @@ namespace AdventOfCode._2020
     {
         public Day13() : base(13, 2020) { }
         protected override void Solve()
-        { 
+        {
+            // p1:
+            // var lines = ReadLines();
+            // var start = lines[0].ToInt();
+            // var ts = lines[0].ToInt();
+            // var buses = lines[1].SplitBy(',').Where(x => x != "x")
+            //     .Select(int.Parse).ToArray();
+            // while (true)
+            // {
+            //     foreach (var bus in buses)
+            //     {
+            //         if (ts % bus == 0)
+            //         {
+            //             var res = (ts - start) * bus;
+            //             Console.WriteLine(res);
+            //             return;
+            //         }
+            //     }
+            //
+            //     ts++;
+            // }
+
+            var lines = ReadLines();
+            var buses = lines[1].SplitBy(',')
+                //.Where(x => x != "x")
+                .Select(x => int.TryParse(x, out var y) ? y : -1)
+                .ToArray();
+            var offsets = buses.WithIndex().Where(x => x.Item != -1)
+                .OrderByDescending(x => x.Item)
+                .ToArray();
+            // solved via Wolfram Alpha
+             // var num =
+             //     525 * 1839692279113 * 438 +
+             //     396 * 2519486192873 * 324 +
+             //     14 * 26608232232049 * 29 +
+             //     21 * 37618535224621 * 2 +
+             //     1 * 47432066152783 * 6 +
+             //     1 * 57417764290211 * 8 +
+             //     14 * 64172795383177 * 15 +
+             //     10 * 83918270885693 * 4;
+             var lcm = 1090937521514009; // Wolfram Alpha
+             // var num = 776257269215190994 % lcm;
+             var num = 600691418730595;
+            for (var ts = num;;)
+            {
+                var all = true;
+                for (var i = 0; i < offsets.Length; i++)
+                {
+                    var offset = (ts + offsets[i].Index) % offsets[i].Item;
+                    if (offset != 0)
+                    {
+                        ts += offsets[i].Item - offset;
+                        if (i >= 1)
+                        {
+                            all = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (all)
+                {
+                    Console.WriteLine(ts);
+                    return;
+                }
+            }
         }
     }
 }
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -11,7 +11,7 @@ namespace AdventOfCode
     {
         static void Main()
         {
-            var day = new Day12();
+            var day = new Day13();
             day.Run();
         }
     }
