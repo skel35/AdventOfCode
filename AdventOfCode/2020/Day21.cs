c4257f39314be011a6244bc0904742bf0efe23c0
Taras Petruk <taras.petruk@gmail.com>
2020-12-24 19:56:13 +0200

    day 21

--- AdventOfCode/2020/Day21.cs
+++ AdventOfCode/2020/Day21.cs
@@ -1,16 +1,65 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
-using static System.Math;
 
 namespace AdventOfCode._2020
 {
     public class Day21 : Solution
     {
-        public Day21() : base(21, 2020) { }
+        public Day21() : base(21, 2020)
+        {
+        }
+
         protected override void Solve()
-        { 
+        {
+            var input =
+                ReadLines()
+                    .Select(line => line.Split(" (contains "))
+                    .Select(s =>
+                        (Ingredients: s[0].SplitBy(' ').ToImmutableHashSet(),
+                            Allergens: s[1][..^1].SplitBy(' ', ',').ToImmutableHashSet()))
+                    .ToArray();
+            // allergen to ingredient
+            var map = new Dictionary<string, string>();
+            var map2 = new Dictionary<string, ImmutableHashSet<string>>();
+
+            foreach (var inp in input)
+            {
+                inp.Allergens.ForEach(a =>
+                {
+                    map2.AddOrUpdate(a,
+                        inp.Ingredients,
+                        (_, cur) => cur.Intersect(inp.Ingredients));
+                });
+            }
+
+            while (true)
+            {
+                var matched = map2.FirstOrDefault(kvp => kvp.Value.Count == 1);
+                if (matched.Equals(default(KeyValuePair<string, ImmutableHashSet<string>>)))
+                    break;
+                var key = matched.Key;
+                var value = matched.Value.Single();
+                map.Add(key, value);
+                map2.Remove(key);
+                map2.ForEach(kvp =>
+                {
+                    map2[kvp.Key] = map2[kvp.Key].Remove(value);
+                });
+                input.Iteri((i, x) =>
+                {
+                    input[i] = (x.Ingredients.Remove(value), x.Allergens.Remove(key));
+                });
+            }
+
+            // p1:
+            // Console.WriteLine(input.Select(x => x.Ingredients.Count).Sum());
+
+            // p2:
+            string.Join(',', map.OrderBy(kvp => kvp.Key).Select(kvp => kvp.Value))
+                .Print();
         }
     }
 }
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -11,7 +11,7 @@ namespace AdventOfCode
     {
         static void Main()
         {
-            var day = new Day20();
+            var day = new Day21();
             day.Run();
         }
     }
