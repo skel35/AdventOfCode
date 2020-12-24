b7ff383d67d25b5def3f551be79d77442bc98e6b
Taras Petruk <taras.petruk@gmail.com>
2020-12-24 21:39:36 +0200

    day 22 (hashing is slow, FIXME)

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -11,7 +11,7 @@ namespace AdventOfCode
     {
         static void Main()
         {
-            var day = new Day21();
+            var day = new Day22();
             day.Run();
         }
     }
